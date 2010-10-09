7281b99e6852d8dc5d88a7ed8d0c2f52a6af6d21
Michael Dukelsky <dukelsky@users.sourceforge.net>
2010-10-09 15:00:58 +0000

    A typo in makefiles is fixed. Thanks to Semen Panevin 2:5025/121.

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
 #undef VERSION
 #endif
-#define VERSION        "1.21"
+#define VERSION        "1.23"
 #define ProgVersion    VERSION""OsType
 #define ProductLO 0xff
 #define ProductHI 0x16
