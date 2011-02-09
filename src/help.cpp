1d94de8ab717faba2bc9128a6858713c5a95ca3d
Stas Degteff <stas_degteff@users.sourceforge.net>
2011-02-09 21:06:51 +0000

    New CL option '-v' (view program version); display banner only for '-v' and '-h' options. (Feature request from Semen Panevin 2:5025/121

--- src/help.cpp
+++ src/help.cpp
@@ -37,9 +37,11 @@ void Hello(void) {
 }
 
 void Help(void) {
-   cout << "Syntax: rntrack -c \"ConfigFile\"   - To select configuration file\n";
-   cout << "                -u                - Unpack outbound\n";
-   cout << "                -t                - Do ScanDir: with expired date of execution\n";
-   cout << "                -h | -?           - To get this text again\n";
+   cout << "Syntax: rntrack -c \"ConfigFile\" - To select configuration file\n";
+   cout << "                -u              - Unpack outbound\n";
+   cout << "                -t              - Do ScanDir: with expired date of execution\n";
+   cout << "                -?              - To view help text again\n";
+   cout << "                -h              - To view program version and command line options\n";
+   cout << "                -v              - To view program version\n";
    cout << flush;
 }
