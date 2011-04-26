4db3a8cde48b510c1a1f625a764f4d3c03c4f7a0
Michael Dukelsky <dukelsky@users.sourceforge.net>
2011-04-26 15:22:06 +0000

    Small editorial changes in Manual_ru

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "1.31"
+#define VERSION               "1.32"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
