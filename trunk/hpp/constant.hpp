03e2f88f75aeb248223463d8ee127f17b45a5d12
Michael Dukelsky <dukelsky@users.sourceforge.net>
2018-08-25 18:29:01 +0000

    fix a bug in not deleting a flag after ScanDir of an empty base, bug report by Alex Barinov 2:5020/715

--- trunk/hpp/constant.hpp
+++ trunk/hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "1.35"
+#define VERSION               "1.36"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
