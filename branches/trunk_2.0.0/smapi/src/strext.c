8200d2ad69516f390182760847503ce009a25e96
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-01-15 19:31:45 +0000

    delete excessive functions

--- branches/trunk_2.0.0/smapi/src/strext.c
+++ branches/trunk_2.0.0/smapi/src/strext.c
@@ -38,27 +38,6 @@ int ctoi(const char *s)
   return (int)res;
 }
 
-char *_fast Strip_Trailing(char *str, char strip)
-{
-    int x;
-    if (str && *str && str[x = strlen(str) - 1] == strip)
-    {
-        str[x] = '\0';
-    }
-    return str;
-}
-
-char *_fast Add_Trailing(char *str, char add)
-{
-    int x;
-    if (str && *str && str[x = strlen(str) - 1] != add)
-    {
-        str[x + 1] = add;
-        str[x + 2] = '\0';
-    }
-    return str;
-}
-
 char *_fast strocpy(char *dst, const char *src)
 {
     const char *orig;
