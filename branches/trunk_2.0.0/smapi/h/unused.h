946f8b7501b37fe81c6be0b2088ad8cf0f9e1a1f
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-01-18 19:23:25 +0000

    change unused() for __GNUC__

--- branches/trunk_2.0.0/smapi/h/unused.h
+++ branches/trunk_2.0.0/smapi/h/unused.h
@@ -8,13 +8,11 @@
 /***  Declarations & defines  ***********************************************/
 
 #ifndef unused
-#   if defined(_MSC_VER)
-#       define unused(x) ((void)(x))
-#   elif defined(PACIFIC) || defined(__GNUC__)
-#       define unused(x) while(0){if(x){};}
-#   elif defined(__HIGHC__)
-#       define unused(x) ((x) = (x))
-#   else
-#       define unused(x) ((void)(x))
-#   endif
+#	if defined(PACIFIC)
+#		define unused(x) while(0){if(x){};}
+#	elif defined(__HIGHC__) || defined(__WATCOMC__)
+#		define unused(x) ((x) = (x))
+#	else
+#		define unused(x) ((void)(x))
+#	endif
 #endif
