67b6f20b81e637904f61244bb5eff1c79ae63f3f
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-01-29 11:13:52 +0000

    set version 2.0.1

--- branches/trunk_2.0.0/hpp/constant.hpp
+++ branches/trunk_2.0.0/hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.0.0"
+#define VERSION               "2.0.1"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
