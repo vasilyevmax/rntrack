3716753b6784b70d7718c68395b59d8ee7786188
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-02-02 11:45:09 +0000

    ver.2.0.4 - fix linux build with Perl

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.0.3"
+#define VERSION               "2.0.4"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
