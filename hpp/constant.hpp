4af756c7896b294f0ebfebba4e6fe176a5a617cc
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-02-05 15:48:25 +0000

    fix rntrack_en.odt and rntrack_ru.odt document properties

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.0.17"
+#define VERSION               "2.0.18"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
