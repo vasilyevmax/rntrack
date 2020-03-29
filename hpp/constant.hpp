b0a087bca7b7c505c2e56d9c78006388c21d5585
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-03-29 20:03:47 +0000

    undo the change committed in r314

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.0.49"
+#define VERSION               "2.0.33"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
