791dd106ad912e6f264d1c0df3b7b4d19fa98446
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-04-26 07:38:11 +0000

    add forgotten comments

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.1.2"
+#define VERSION               "2.1.3"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
