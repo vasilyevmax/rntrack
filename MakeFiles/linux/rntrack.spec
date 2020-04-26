a17a685fa0cf85ea98de6ff61ce869cd131089b0
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-04-26 16:48:16 +0000

    use environment variable %HOME% in configuration sample

--- MakeFiles/linux/rntrack.spec
+++ MakeFiles/linux/rntrack.spec
@@ -1,5 +1,5 @@
 Name: rntrack
-Version: 2.1.6
+Version: 2.1.7
 Release: 1%{?dist}
 Group: Applications/FTN
 Summary: RNtrack - a FTN netmail tracker
--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.1.6"
+#define VERSION               "2.1.7"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
