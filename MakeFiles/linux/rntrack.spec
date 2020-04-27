793a15394d9bc22aab6f009bd8fd412b7435bdbb
Michael Dukelsky <dukelsky@users.sourceforge.net>
2020-04-27 13:32:33 +0000

    add forgotten comments to two more files

--- MakeFiles/linux/rntrack.spec
+++ MakeFiles/linux/rntrack.spec
@@ -1,5 +1,5 @@
 Name: rntrack
-Version: 2.1.7
+Version: 2.1.8
 Release: 1%{?dist}
 Group: Applications/FTN
 Summary: RNtrack - a FTN netmail tracker
--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -66,7 +66,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.1.7"
+#define VERSION               "2.1.8"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
