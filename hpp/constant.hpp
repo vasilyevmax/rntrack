cfd505c8dac655ae4cae6983825ca7f1c8647390
vasilyevmax <35378045+vasilyevmax@users.noreply.github.com>
2022-02-19 16:43:59 +0400

    set version v2.2.1

--- hpp/constant.hpp
+++ hpp/constant.hpp
@@ -70,7 +70,7 @@
 #ifdef VERSION
     #undef VERSION
 #endif
-#define VERSION               "2.2.0"
+#define VERSION               "2.2.1"
 #ifdef __PERL_VERSION__
     #define Perl              "/Perl"
 #else
