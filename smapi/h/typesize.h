1da2d26f4312ab5fc76ae86d685044d2d75e9c7f
vasilyevmax <35378045+vasilyevmax@users.noreply.github.com>
2023-02-18 10:32:38 +0400

    moved from husky (OS/2 compilation with modern gcc)

--- smapi/h/typesize.h
+++ smapi/h/typesize.h
@@ -293,7 +293,7 @@ typedef unsigned long       ULONG;              /*  4 byte */
  * define HAS_STDINT_H if you have one in your system or
  * define USE_PSTDINT_H if it works as replacement */
 #include "pstdint.h"
-typedef   int8_t            CHAR;               /*  1 byte */
+/* typedef   int8_t            CHAR; */              /*  1 byte */
 typedef  uint8_t           UCHAR;               /*  1 byte */
 typedef   int16_t           INT16;              /*  2 byte */
 typedef  uint16_t          UINT16;              /*  2 byte */
